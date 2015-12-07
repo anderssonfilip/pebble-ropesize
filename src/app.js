e3ca7eedc747d07f1318dd7c5394b5f6a991f279
Filip Andersson <trade-spoke@yandex.com>
2015-12-07 06:30:34 -0800

    Show ropesizes 4mm (5/32") to 26mm (1 1/32")

--- src/app.js
+++ src/app.js
@@ -6,50 +6,56 @@ var window = new UI.Window();
 var rope;
 var text;
 
-function drawRope(size){
- rope = new UI.Rect({
-  position: new Vector2(72-size/2, 0),
-  size: new Vector2(size,168),
-  backgroundColor: 'white',
-});
+var index = 0;
+var displayInches = false;
+var ropeSizes = [[4, "5/32"], [5, "3/16"], [6, "7/32"], [7, "1/4"], [8, "5/16"], [10, "13/32"], [12, "1/2"], [14, "9/16"], [16, "5/8"], [18, "23/32"], [20, "13/16"], [22, "7/8"], [24, "1"], [26, "1 1/32"]];
 
-window.add(rope);
-window.show(rope);
+drawVerticalRope(index);
+drawSizeLabel(index);
 
-  
-    text = new UI.Text({
-      text: (size/2).toString() + " mm",
-  position: new Vector2(0, 0),
-      size: new Vector2(72, 15),
-  color: 'red',
-      textAlign: "left"
-});
+function drawVerticalRope(index) {
+  window.remove(rope);
+  rope = new UI.Rect({
+    position: new Vector2(72 - (ropeSizes[index][0] * 5) / 2, 0),
+    size: new Vector2(ropeSizes[index][0] * 5, 168),
+    backgroundColor: 'white',
+  });
 
-window.add(text);
-window.show(text);
+  window.add(rope);
+  window.show(rope);
 }
 
-var size = 2;
-drawRope(size);
+function drawSizeLabel(index) {
+  window.remove(text);
+  text = new UI.Text({
+    text: displayInches ? ropeSizes[index][1] + '"' : ropeSizes[index][0].toString() + " mm",
+    position: new Vector2(0, 0),
+    size: new Vector2(72, 15),
+    color: 'red',
+    textAlign: "left"
+  });
 
-window.on('click', 'up', function(e) {
-  if(size < 144){
-    window.remove(rope);
-    window.remove(text);
-    size=size+2;
-    drawRope(size);
+  window.add(text);
+  window.show(text);
+}
+
+window.on('click', 'up', function (e) {
+  if (index < ropeSizes.length-1) {
+    index++;
+    drawVerticalRope(index);
+    drawSizeLabel(index);
   }
 });
 
-window.on('click', 'select', function(e) {
-
+window.on('click', 'select', function (e) {
+  displayInches = !displayInches;
+  drawSizeLabel(index);
 });
 
-window.on('click', 'down', function(e) {
-  if(size > 4){
-    window.remove(rope);
-    window.remove(text);
-    size=size-2;
-    drawRope(size);
+window.on('click', 'down', function (e) {
+  if (index > 0) {
+    index--;
+    drawVerticalRope(index);
+    drawSizeLabel(index);
   }
 });
